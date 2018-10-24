790f6363181395a74d9393a28d896716e68fb103
VictorBBulhoes <44437190+VictorBBulhoes@users.noreply.github.com>
2018-10-24 10:41:53 -0300

    Add files via upload
    Talvez funcione

--- dominoModel.c
+++ dominoModel.c
@@ -1,2 +1,22 @@
 
 #include "dominoModel.h"
+
+int gerarPecas(tipo_Peca *Ppeca){
+	int i, j, k;
+	int id = 0;
+	k = 0;
+
+	for(i = 0; i > 7; i++){
+		for(j = k; j > 7; j ++){
+
+			Ppeca[id]->num1 = i;
+			Ppeca[id]->num2 = j;
+			//Ppeca->ID = id;
+
+			id++;
+			k++;
+		}
+	}
+
+	
+}
