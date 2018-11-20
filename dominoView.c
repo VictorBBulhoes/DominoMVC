fcf9cb8dc07b7fbcd229245d1ba628500a937084
GabsRSilva <42253364+GabsRSilva@users.noreply.github.com>
2018-11-20 17:58:43 -0200

    Update dominoView.c

--- dominoView.c
+++ dominoView.c
@@ -21,12 +21,12 @@ int mostrarMenuPrincipal()
         printf(" 3 - Carregar jogo\n");
         printf(" 4 - Sair do jogo\n");
         printf("\n");
-        printf(" Entre com uma das opcoes: ");
+        printf(" Entre com uma das opções: ");
         scanf("%d", &Op);
         if (Op < 1 || Op > 4)
         {
             limparTelaHibrido();
-            printf("\n*******************OPCAO INVALIDA!*******************\n");
+            printf("\n*******************OPÇÃO INVÁLIDA!*******************\n");
             printf("Pressione Enter para continuar...");
             setbuf(stdin,NULL);
             getchar();
@@ -58,7 +58,7 @@ int modoJogo()
         if (modo < 1 || modo > 2)
         {
             limparTelaHibrido();
-            printf("\n*******************OPCAO INVALIDA!*******************\n");
+            printf("\n*******************OPÇÃO INVÁLIDA!*******************\n");
             printf("Pressione Enter para continuar...");
             setbuf(stdin,NULL);
             getchar();
@@ -79,17 +79,17 @@ void regrasJogo()
 {
 	printf("\n\t\t\t Regras do Jogo\n");
 	printf("----------------------------------------------------------------\n\n");
-	printf(" Cada jogador deve iniciar o jogo com 7 pecas.\n");
-	printf(" O jogo jogador que possuir a peca 6:6 faz a primeira jogada.\n Caso nenhum jogador possua a peca 6:6, inicia aquele com o dobre(1:1, 2:2, 3:3, 4:4, 5:5) mais alto.\n");
-	printf(" O jogo ocorre em sentido anti-horario.\n");
-	printf(" Os jogadores devem colocar pecas que tenham os mesmos numeros\n das pecas que se encontram nas pontas do jogo.\n");
+	printf(" Cada jogador deve iniciar o jogo com 7 peças.\n");
+	printf(" O jogador que possuir a peça 6:6 faz a primeira jogada.\n Caso nenhum jogador possua a peça 6:6, inicia aquele com o dobre\n(1:1, 2:2, 3:3, 4:4, 5:5) mais alto.\n");
+	printf(" O jogo ocorre em sentido anti-horário.\n");
+	printf(" Os jogadores devem colocar peças que tenham os mesmos números\n das peças que se encontram nas pontas do jogo.\n");
     printf("\nAperte a tecla ENTER para retornar ao menu...");
 }
 
 int menuJogada(int acaoJogo)
 {
-	printf("1) Jogar Peca   2) Comprar Peca     3) Salvar   4) Menu Principal\n");
-	printf(">");
+	printf("1) Jogar Peça   2) Comprar Peça     3) Salvar   4) Menu Principal\n");
+	printf(">> ");
 	scanf("%d", &acaoJogo);
 	return acaoJogo;
 
@@ -99,7 +99,7 @@ void mostrarPecas(tipo_Peca pecas[28])
 {
 	int i, j, k = 0, o = 0;
 
-	printf("Pecas do jogo: \n");
+	printf("Peças do jogo: \n");
 	for(i = 0; i < 7; i++)
 	{
 		printf("\n");
