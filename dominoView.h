e0359676d7476262cc76ab9d104925aac562bd36
Breno Amaral <26623301+BrAmaral@users.noreply.github.com>
2018-11-20 23:54:12 -0200

    Add files via upload

--- dominoView.h
+++ dominoView.h
@@ -10,7 +10,6 @@ void mensagemDeAdeus();                     // Imprime uma breve mensagem de agr
 void mostrarMesa(tipo_Peca pecas[28], int pecasMesa[56]);      // Imprime as pecas na mesa
 void mostrarPecasJogador(tipo_Peca pecas[28], int pecasJogador[21]);       // Imprime as pecas na mao do jogador
 void pausaEstrategica();                    // Pausa o programa e espera o usuario apertar enter.
-void escolhaPeca();                         // Jogador escolhe uma peca que esta em sua mao.
 void mensagemDePrimeiro(int comeco);        // Imprime uma mensagem dizendo quem comecou o jogo.
 void mensagemFinalJogo(int vencedor);       // Imprime uma mensagem de fim de jogo.
 void modoIndisponivel();                    // Imprime uma mensagem que o modo está indisponivel no momento.
@@ -19,5 +18,6 @@ void modoIndisponivel();                    // Imprime uma mensagem que o modo e
 int mostrarMenuPrincipal();             // Imprime o menu principal.
 int modoJogo();                         // Imprime menu de selecao do modo de jogo (Singleplayer ou Multiplayer) e retorna 1 para Singleplayer e 2 para Multiplayer.
 int menuJogada(int acaoJogo);           // Imprime o menu de jogada (Opcoes que o jogador tem durante o jogo).
+int escolhaPeca();                         // Jogador escolhe uma peca que esta em sua mao.
 
 #endif // DOMINOVIEW_H_INCLUDED
